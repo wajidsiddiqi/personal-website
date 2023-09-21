0e2c057b7a8e4615f92c4e57bf1651cbaaae1312
wajid <wajidsiddiqi12@gmail.com>
2023-09-21 22:08:25 +0500

    fonts

---
+++ src/app/fonts.js
@@ -0,0 +1,12 @@
+import { Sora, Gloock } from "next/font/google";
+
+export const sora = Sora({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const gloock = Gloock({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
