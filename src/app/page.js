09cd3d69a67cd9ada4e904c55bf8e818a7697bb6
wajid <wajidsiddiqi12@gmail.com>
2023-09-22 23:05:31 +0500

    building home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,68 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { gloock } from "./fonts";
+/* import Footer from "./components/Footer";
+import EnlargeImage from "./components/EnlargeImage";
+import { TotalSupply } from "./components/getterFuntions";
+import Mint from "./components/Mint"; */
+import {
+  MainPageWrapper,
+  MainH1Title,
+  ParaBig,
+  ParaMid,
+  ParaSm,
+  StyledButton,
+  ImageContainer,
+  ImageStyle,
+  Center,
+  LeftSide,
+  RightSide,
+  Container,
+  Box,
+  ChildContainer,
+  BoxContainer,
+  Icon,
+  QuantityBox,
+  BoxContainer_2,
+} from "@/app/styles/styles.js";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <MainPageWrapper>
+      <Container>
+        <MainH1Title className={gloock.className}>
+          Hello There! I'm Wajid, Your Full-Stack Web 3.0 Developer.
+        </MainH1Title>
+        <ParaBig>
+          Welcome to my digital space, where I bring your Web 3.0 dreams to
+          life. Explore my portfolio and let's build the future of the web
+          together. I create cutting-edge web experiences that stand out in the
+          digital landscape.
+        </ParaBig>
+        <a href="mailto:wajidsiddiqi12@gmail.com" target="_blank">
+          <StyledButton>Let's Build Together</StyledButton>
+        </a>
+        <Icon>
+          <Link href="https://github.com/wajidsiddiqi" target="_blank">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src="/assets/icons/github.svg"
+              width="36"
+              height="36"
+              alt="My Github"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          </Link>
+          <Link href="https://twitter.com/abdulwajidsid" target="_blank">
+            <Image
+              src="/assets/icons/twitter.svg"
+              width="36"
+              height="36"
+              alt="My Twitter"
+            />
+          </Link>
+        </Icon>
+      </Container>
+    </MainPageWrapper>
+  );
 }
