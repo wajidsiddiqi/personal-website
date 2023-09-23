e506a7187cfdeeb6376f4373f2ad4a724b51710f
wajid <wajidsiddiqi12@gmail.com>
2023-09-23 22:46:11 +0500

    added skill component

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { gloock } from "./fonts";
+import Skills from "./components/Skills";
 /* import Footer from "./components/Footer";
 import EnlargeImage from "./components/EnlargeImage";
 import { TotalSupply } from "./components/getterFuntions";
@@ -11,58 +12,49 @@ import {
   MainPageWrapper,
   MainH1Title,
   ParaBig,
-  ParaMid,
-  ParaSm,
   StyledButton,
-  ImageContainer,
-  ImageStyle,
-  Center,
-  LeftSide,
-  RightSide,
   Container,
-  Box,
-  ChildContainer,
-  BoxContainer,
   Icon,
-  QuantityBox,
-  BoxContainer_2,
 } from "@/app/styles/styles.js";
 
 export default function Home() {
   return (
-    <MainPageWrapper>
-      <Container>
-        <MainH1Title className={gloock.className}>
-          Hello There! I'm Wajid, Your Full-Stack Web 3.0 Developer.
-        </MainH1Title>
-        <ParaBig>
-          Welcome to my digital space, where I bring your Web 3.0 dreams to
-          life. Explore my portfolio and let's build the future of the web
-          together. I create cutting-edge web experiences that stand out in the
-          digital landscape.
-        </ParaBig>
-        <a href="mailto:wajidsiddiqi12@gmail.com" target="_blank">
-          <StyledButton>Let's Build Together</StyledButton>
-        </a>
-        <Icon>
-          <Link href="https://github.com/wajidsiddiqi" target="_blank">
-            <Image
-              src="/assets/icons/github.svg"
-              width="36"
-              height="36"
-              alt="My Github"
-            />
-          </Link>
-          <Link href="https://twitter.com/abdulwajidsid" target="_blank">
-            <Image
-              src="/assets/icons/twitter.svg"
-              width="36"
-              height="36"
-              alt="My Twitter"
-            />
-          </Link>
-        </Icon>
-      </Container>
-    </MainPageWrapper>
+    <React.Fragment>
+      <MainPageWrapper>
+        <Container>
+          <MainH1Title className={gloock.className}>
+            Hello There! I'm Wajid, Your Full-Stack Web 3.0 Developer.
+          </MainH1Title>
+          <ParaBig>
+            Welcome to my digital space, where I bring your Web 3.0 dreams to
+            life. Explore my portfolio and let's build the future of the web
+            together. I create cutting-edge web experiences that stand out in
+            the digital landscape.
+          </ParaBig>
+          <a href="mailto:wajidsiddiqi12@gmail.com" target="_blank">
+            <StyledButton>Let's Build Together</StyledButton>
+          </a>
+          <Icon>
+            <Link href="https://github.com/wajidsiddiqi" target="_blank">
+              <Image
+                src="/assets/icons/github.svg"
+                width="36"
+                height="36"
+                alt="My Github"
+              />
+            </Link>
+            <Link href="https://twitter.com/abdulwajidsid" target="_blank">
+              <Image
+                src="/assets/icons/twitter.svg"
+                width="36"
+                height="36"
+                alt="My Twitter"
+              />
+            </Link>
+          </Icon>
+        </Container>
+      </MainPageWrapper>
+      <Skills />
+    </React.Fragment>
   );
 }
