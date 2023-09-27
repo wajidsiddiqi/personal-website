03a92dbd405955f27f979479685fed280d02bdd7
wajid <wajidsiddiqi12@gmail.com>
2023-09-27 10:58:27 +0500

    added tag

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -15,7 +15,7 @@ import {
 
 export default function Projects() {
   return (
-    <PageWrapper>
+    <PageWrapper id="projects">
       <MainH1Title className={gloock.className}>Projects</MainH1Title>
       <Container style={{ flexDirection: "row", flexWrap: "wrap" }}>
         <Box>
@@ -95,45 +95,6 @@ export default function Projects() {
                 width="36"
                 height="36"
                 alt="Live Demo"
-              />
-            </Link>
-          </Icon>
-        </Box>
-        <Box>
-          <ImageContainer>
-            <Image
-              src="/assets/project_images/project_1.png"
-              alt="Project 1"
-              width={0}
-              height={0}
-              layout="responsive"
-              style={ImageStyle}
-            />
-          </ImageContainer>
-          <ParaMid>
-            I've crafted an exciting website for Beanz Pixel NFTs on the Polygon
-            blockchain. By leveraging the power of Next.js, React, Styled
-            Components, and some other intriguing technologies, I've brought
-            this project to life.
-          </ParaMid>
-          <Icon>
-            <Link
-              href="https://github.com/wajidsiddiqi/project-2"
-              target="_blank"
-            >
-              <Image
-                src="/assets/icons/github_v2.svg"
-                width="36"
-                height="36"
-                alt="My Github"
-              />
-            </Link>
-            <Link href="https://project-2-nu.vercel.app/" target="_blank">
-              <Image
-                src="/assets/icons/external_link.svg"
-                width="36"
-                height="36"
-                alt="My Twitter"
               />
             </Link>
           </Icon>
